f24d244e7296fa251ff09f08359aae901ed26241
NelcifranMagalhaes <nelcifranpires@gmail.com>
2024-12-11 15:04:51 -0300

    cpf validator

--- university/validators.py
+++ university/validators.py
@@ -1,3 +1,5 @@
+import re
+
 def cpf_invalid(cpf):
     return len(cpf) !=11
 
@@ -5,4 +7,7 @@ def name_invalid(name):
     return not name.isalpha()
 
 def phone_invalid(phone):
-    return len(phone) !=13
+    # 86 99999-9999
+    regex_model = '[0-9]{2} [0-9]{5}-[0-9]{4}'
+    response = re.findall(regex_model,phone)
+    return not response
